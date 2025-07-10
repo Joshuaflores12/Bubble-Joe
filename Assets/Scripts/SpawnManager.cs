9dde583c3672bfa21bac339911c5d4a145f06943
MrBearA <110599403+MrBearA@users.noreply.github.com>
2025-07-10 22:50:03 +0800

    Reapply "add some more feature"
    This reverts commit 578b67990f812d46bb29ef02091e46b676b1d924.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,69 @@
+// SpawnManager.cs
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] Transform spawnPos;
-    [SerializeField] GameObject playerPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Tooltip("Drag in all your checkpoint Transforms here (in order or arbitrary).")]
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    [Tooltip("Your player prefab.")]
+    [SerializeField] private GameObject playerPrefab;
+
+    private Transform currentSpawnPoint;
+    private GameObject playerInstance;
 
     private void Start()
     {
-        Instantiate(playerPrefab, spawnPos.position, Quaternion.identity);
+        if (spawnPoints.Count == 0)
+        {
+            Debug.LogError("SpawnManager: No spawnPoints assigned!");
+            return;
+        }
+
+        // Default to first one
+        currentSpawnPoint = spawnPoints[0];
+        SpawnPlayer();
     }
 
-    public void Respawn() 
+    /// <summary>
+    /// Instantiates (or re-instantiates) the player at the current checkpoint.
+    /// </summary>
+    private void SpawnPlayer()
     {
-        Instantiate(playerPrefab, spawnPos.position, Quaternion.identity);
+        // Clean up old instance if any
+        if (playerInstance != null)
+            Destroy(playerInstance);
+
+        playerInstance = Instantiate(
+            playerPrefab,
+            currentSpawnPoint.position,
+            currentSpawnPoint.rotation
+        );
+    }
+
+    /// <summary>
+    /// Call this to respawn the player at the last checkpoint.
+    /// </summary>
+    public void Respawn()
+    {
+        SpawnPlayer();
+    }
+
+    /// <summary>
+    /// Switches which Transform will be used on the next spawn/respawn.
+    /// The passed-in transform must be one of those in spawnPoints.
+    /// </summary>
+    public void SetCheckpoint(Transform checkpointTransform)
+    {
+        if (spawnPoints.Contains(checkpointTransform))
+        {
+            currentSpawnPoint = checkpointTransform;
+            Debug.Log($"Checkpoint set to: {checkpointTransform.name}");
+        }
+        else
+        {
+            Debug.LogWarning($"SpawnManager: {checkpointTransform.name} not in spawnPoints list!");
+        }
     }
 }
