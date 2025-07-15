3e74457d5c3fcdbd57d53ff44f5744c638dd41e7
Aesir77 <134356658+Aesir77@users.noreply.github.com>
2025-07-15 22:54:02 +0800

    test

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [Tooltip("World-space distance from start to each patrol edge")]
+    [SerializeField] private float patrolDistance = 3f;
+
+    [Tooltip("Speed of movement in units/sec")]
+    [SerializeField] private float speed = 2f;
+
+    private Vector3 leftEdge;
+    private Vector3 rightEdge;
+    private bool movingRight = true;
+
+    private void Start()
+    {
+        // define patrol bounds based on where the enemy started
+        leftEdge  = transform.position + Vector3.left  * patrolDistance;
+        rightEdge = transform.position + Vector3.right * patrolDistance;
+    }
+
+    private void Update()
+    {
+       
+
+        Vector3 target = movingRight ? rightEdge : leftEdge;
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.05f)
+            movingRight = !movingRight;
+    }
+}
---
+++ Assets/Scripts/MainMenu/MenuActions.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Audio;
+using TMPro;
+
+public class MenuActions : MonoBehaviour
+{
+    public GameObject settingsPanel, Main_menuPanel;
+    public Slider Volume;
+    public AudioMixer mainAudio;
+
+    private void Start()
+    {
+        settingsPanel.SetActive(false);
+        Main_menuPanel.SetActive(true);
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+   
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    public void Open_Settings()
+    {
+
+        settingsPanel.SetActive(true);
+        Main_menuPanel.SetActive(false);
+    }
+
+    public void Close_Settings()
+    {
+
+        settingsPanel.SetActive(false);
+        Main_menuPanel.SetActive(true);
+    }
+
+    public void SetVolume()
+    {
+        mainAudio.SetFloat("Volume", Volume.value);
+    }
+}
